eca4ba60538f89257c7bb9ca7dc837afa9dc1019
@SilvaJosias <72473790+krkn12@users.noreply.github.com>
2025-03-20 00:54:17 -0300

    ajuste da api do front

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://158.69.35.122:5000' // Mude de localhost para o IP do VPS
+  baseURL: 'https://158.69.35.122:5000' // Muda de http pra https
 });
 
 // Interceptor para incluir o token em todas requisições
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
