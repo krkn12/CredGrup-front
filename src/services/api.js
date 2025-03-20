d80a156b7223fb37725b972b778f69e560fd637b
@SilvaJosias <72473790+krkn12@users.noreply.github.com>
2025-03-20 00:57:30 -0300

    requisiçao de certificados

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
+import https from 'https';
 
 const api = axios.create({
-  baseURL: 'https://158.69.35.122:5000' // Muda de http pra https
+  baseURL: 'https://158.69.35.122:5000',
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false // Ignora certificados inválidos (só pra teste)
+  })
 });
 
-// Interceptor para incluir o token em todas requisições
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) {
