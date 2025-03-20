15c7a9b34d38772cca9007b3155c8f1c34f5ed89
@SilvaJosias <72473790+krkn12@users.noreply.github.com>
2025-03-20 01:01:19 -0300

    ja

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
-import https from 'https';
 
 const api = axios.create({
-  baseURL: 'https://158.69.35.122:5000',
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false // Ignora certificados inválidos (só pra teste)
-  })
+  baseURL: 'https://158.69.35.122:5000'
 });
 
 api.interceptors.request.use(config => {
