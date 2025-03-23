785819d48f569e6e0f3814f629b565691ab12a99
@SilvaJosias <72473790+krkn12@users.noreply.github.com>
2025-03-23 10:57:10 -0300

    Atualiza a configuração da API para utilizar um novo endpoint, mudando a URL base para "https://credgrup.click/api" e adicionando um comentário sobre o uso do proxy Cloudflare.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://158.69.35.122:5000/api",
+  baseURL: "https://credgrup.click/api", // Porta 80 com proxy Cloudflare
   headers: {
     "Content-Type": "application/json",
   },
