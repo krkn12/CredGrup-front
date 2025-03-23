0ccc2cb209fdcaa4cd24990c6dd35b162b7ff1cf
@SilvaJosias <72473790+krkn12@users.noreply.github.com>
2025-03-23 00:08:28 -0300

    Refatora o componente Login para melhorar a legibilidade do código, unificando a formatação de mensagens de erro e simplificando a estrutura de alguns elementos JSX. Atualiza a configuração da API para utilizar um novo endpoint e define o cabeçalho de conteúdo como JSON.

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,14 @@
 import axios from "axios";
+import https from "https";
 
 const api = axios.create({
-  baseURL: "https://158.69.35.122:5000/api", // Ajuste para o IP e porta da VPS
+  baseURL: "https://158.69.35.122:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false, // Ignora certificado inválido
+  }),
 });
 
 export default api;
