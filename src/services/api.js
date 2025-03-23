4400e0f2c963e211ce2abc2206d930fb8279e3fe
@SilvaJosias <72473790+krkn12@users.noreply.github.com>
2025-03-23 00:31:50 -0300

    Remove a configuração do agente HTTPS na API, simplificando a criação da instância do axios e mantendo a definição do cabeçalho de conteúdo como JSON.

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,10 @@
 import axios from "axios";
-import https from "https";
 
 const api = axios.create({
   baseURL: "https://158.69.35.122:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false, // Para certificados autoassinados
-  }),
 });
 
 export default api;
