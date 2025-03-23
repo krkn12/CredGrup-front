fa95bbbe7dc5bc291cf3f5a3b52bbccbb8031f05
@SilvaJosias <72473790+krkn12@users.noreply.github.com>
2025-03-23 00:29:16 -0300

    Atualiza a configuração da API para utilizar um novo endpoint na porta 5000 e ajusta a descrição do agente HTTPS para esclarecer o uso com certificados autoassinados.

--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,12 @@ import axios from "axios";
 import https from "https";
 
 const api = axios.create({
-  baseURL: "https://158.69.35.122:3000/api",
+  baseURL: "https://158.69.35.122:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
   httpsAgent: new https.Agent({
-    rejectUnauthorized: false, // Ignora certificado inválido
+    rejectUnauthorized: false, // Para certificados autoassinados
   }),
 });
 
