66729a427fbe0d66fe82dfe3d61fd351e140a553
@SilvaJosias <72473790+krkn12@users.noreply.github.com>
2025-03-27 01:42:15 -0300

    Remove arquivos de estilos não utilizados 'admin-panel.css' e 'admin-sidebar.css', e corrige a importação de estilos no componente AdminSidebar para './styles/admin/admin-sidebar.css', aprimorando a organização da estrutura de estilos.

--- src/components/Admin/AdminSidebar.jsx
+++ src/components/Admin/AdminSidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';// Importação correta (caminho relativo)
 import './styles/admin/admin-sidebar.css';
 
 const AdminSidebar = () => {
